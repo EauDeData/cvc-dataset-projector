ba1e54e0d074889aa10f6f92f11647b8a0adac46
EauDeData <amolina@cvc.uab.cat>
2023-03-29 18:05:13 +0200

    quan la derrota és segura

--- create_image.py
+++ create_image.py
@@ -6,7 +6,7 @@ import cv2
 
 dataset = ZippedDataloader('/home/adri/Pictures/zipper.zip')
 model = Resnet(resnet='50')
-projector = PCAProjector(dataset, model)
+projector = PCAProjector(dataset, model, imsize = 30, mapsize = 300)
 
 image = projector.place_images()
 cv2.imwrite('tmp.png', image)
